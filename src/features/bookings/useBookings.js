1dc14e2226f67957b80b9efd0e4b3cf62bed35de
Gilbert Haro <gilbertharocode@gmail.com>
2025-01-29 14:23:42 -0800

    Building Paginating Component, API-Side Pagination Bookings, Prefetching With React Query

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,18 +1,52 @@
-import { useQuery } from '@tanstack/react-query';
-import { useParams } from 'react-router-dom';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { useParams, useSearchParams } from 'react-router-dom';
 import { getBookings } from '../../services/apiBookings';
+import { PAGE_SIZE } from '../../utils/constants';
 
 export function useBookings() {
   // const { bookingId } = useParams();
+  const queryClient = useQueryClient();
+  const [searchParams] = useSearchParams();
 
+  // Filter
+  const filterValue = searchParams.get('status');
+  const filter =
+    !filterValue || filterValue === 'all'
+      ? null
+      : { field: 'status', value: filterValue };
+
+  //// SORT
+  const sortByRaw = searchParams.get('sortBy') || 'startDate-desc';
+  const [field, direction] = sortByRaw.split('-');
+  const sortBy = { field, direction };
+
+  /// PAGINATION
+  const page = !searchParams.get('page') ? 1 : Number(searchParams.get('page'));
+
+  /// QUERY
   const {
     isPending,
-    data: bookings,
+    data: { data: bookings, count } = {},
     error,
   } = useQuery({
-    queryKey: ['bookings'],
-    queryFn: getBookings,
+    queryKey: ['bookings', filter, sortBy, page],
+    queryFn: () => getBookings({ filter, sortBy, page }),
   });
 
-  return { isPending, error, bookings };
+  /// PRE-FETCHING
+  const pageCount = Math.ceil(count / PAGE_SIZE);
+
+  if (page < pageCount)
+    queryClient.prefetchQuery({
+      queryKey: ['bookings', filter, sortBy, page + 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
+    });
+
+  if (page > 1)
+    queryClient.prefetchQuery({
+      queryKey: ['bookings', filter, sortBy, page - 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page - 1 }),
+    });
+
+  return { isPending, error, bookings, count };
 }
--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,11 +1,13 @@
+import { PAGE_SIZE } from '../utils/constants';
 import { getToday } from '../utils/helpers';
 import supabase from './supabase';
 
 export async function getBookings({ filter, sortBy, page }) {
-  const { data, error } = await supabase
+  let query = supabase
     .from('bookings')
     .select(
-      'id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)'
+      'id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)',
+      { count: 'exact' }
     );
   // let query = supabase
   //   .from('bookings')
@@ -15,28 +17,34 @@ export async function getBookings({ filter, sortBy, page }) {
   //   );
 
   // FILTER
-  // if (filter) query = query[filter.method || 'eq'](filter.field, filter.value);
+  if (filter) query = query[filter.method || 'eq'](filter.field, filter.value);
 
   // SORT
+
   // if (sortBy)
   //   query = query.order(sortBy.field, {
   //     ascending: sortBy.direction === 'asc',
   //   });
 
-  // if (page) {
-  //   const from = (page - 1) * PAGE_SIZE;
-  //   const to = from + PAGE_SIZE - 1;
-  //   query = query.range(from, to);
-  // }
+  if (sortBy)
+    query = query.order(sortBy.field, {
+      ascending: sortBy.direction === 'asc',
+    });
 
-  // const { data, error, count } = await query;
+  if (page) {
+    const from = (page - 1) * PAGE_SIZE;
+    const to = from + PAGE_SIZE - 1;
+    query = query.range(from, to);
+  }
+
+  const { data, error, count } = await query;
 
   if (error) {
     console.error(error);
     throw new Error('Bookings could not be loaded');
   }
 
-  return data;
+  return { data, count };
 }
 
 export async function getBooking(id) {
